eb2c2aed8c5fa119a262e7ab89d99fbaa574b184
Riazul Hasan <riazul701@gmail.com>
2020-06-18 22:15:54 +0600

    Eloquent ORM-Getting Started code from https://vegibit.com
    Tutorial Web Address:
    https://vegibit.com/how-to-get-started-with-eloquent-in-laravel/

---
+++ resources/views/games/show.blade.php
@@ -0,0 +1,18 @@
+<table border="1">
+    <tr>
+        <th>ID</th>
+        <th>Title</th>
+        <th>Publisher</th>
+        <th>ReleaseDate</th>
+        <th>CreatedAt</th>
+        <th>UpdatedAt</th>
+    </tr>
+    <tr>
+        <td>{{ $game->id }}</td>
+        <td>{{ $game->title }}</td>
+        <td>{{ $game->publisher }}</td>
+        <td>{{ $game->releasedate }}</td>
+        <td>{{ $game->created_at }}</td>
+        <td>{{ $game->updated_at }}</td>
+    </tr>
+</table>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Game;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +15,59 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+//    return view('welcome');
+//    return Game::nintendo()->get();
+    return Game::nintendo()->where('title', 'like', '%Mario%')->get();
+});
+
+Route::get('games', function () {
+//    $games = App\Game::all();
+    $games = Game::all();
+//    return view('games.index', ['games' => $games]);
+    return $games;
+});
+
+Route::get('games/{id}', function ($id) {
+    $game = Game::find($id);
+    return view('games.show', ['game' => $game]);
+});
+
+Route::get('games_where', function () {
+    $games = Game::where('id', '>', 3)->get();
+    return $games;
+});
+
+Route::get('games_where2', function () {
+    $games = Game::where('title', 'like', '%Mega%')->get();
+    return $games;
+});
+
+Route::get('games_where3', function () {
+    $games = Game::where('publisher', '=', 'Nintendo')->get();
+    return $games;
+});
+
+Route::get('games_where4', function () {
+    $games = Game::where('releasedate', '=', 1989)->get();
+    return $games;
+});
+
+Route::get('games_orderby', function () {
+    $games = Game::orderBy('id', 'desc')->get();
+    return $games;
+});
+
+Route::get('games_take', function () {
+    $games = Game::orderBy('releasedate', 'desc')->take(3)->get();
+    return $games;
+});
+
+Route::get('games_pluck', function () {
+    $games = Game::pluck('title');
+    return $games;
+});
+
+Route::get('games_take2', function () {
+    $games = Game::where('publisher', '=', 'Nintendo')->orderBy('releasedate', 'desc')->take(3)->get();
+    return $games;
 });
