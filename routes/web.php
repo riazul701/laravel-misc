a0bb0c815c86908d0a298bb3e9178bd0204a7d12
Riazul Hasan <riazul701@gmail.com>
2020-07-11 13:34:47 +0600

    Multi auth example using Auth guard.
    Tutorial Web Address:
    https://www.itsolutionstuff.com/post/laravel-52-multi-auth-example-using-auth-guard-from-scratchexample.html

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::group(['middleware' => ['web']], function () {
+    Route::get('web-login', 'Auth\AuthController@webLogin');
+    Route::post('web-login', ['as'=>'web-login','uses'=>'Auth\AuthController@webLoginPost']);
+    Route::get('admin-login', 'AdminAuth\AuthController@adminLogin');
+    Route::post('admin-login', ['as'=>'admin-login','uses'=>'AdminAuth\AuthController@adminLoginPost']);
+});
