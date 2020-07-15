696e1643b2c876a292fb2f7fd308e0d19f3bf9ae
Riazul Hasan <riazul701@gmail.com>
2020-07-15 19:05:02 +0600

    Form Validation with Error Messages.
    Tutorial Web Address:
    https://www.itsolutionstuff.com/post/laravel-6-form-validation-with-error-messagesexample.html

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\User;
+
+class HomeController extends Controller
+{
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('createUser');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'password' => 'required|min:5',
+            'email' => 'required|email|unique:users'
+        ], [
+            'name.required' => 'Name is required',
+            'password.required' => 'Password is required'
+        ]);
+
+        $input = $request->all();
+        $input['password'] = bcrypt($input['password']);
+        $user = User::create($input);
+
+        return back()->with('success', 'User created successfully.');
+    }
+}
