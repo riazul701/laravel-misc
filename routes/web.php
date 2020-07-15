58226850a6096ba787a288feffe6bae237e1c87a
Riazul Hasan <riazul701@gmail.com>
2020-07-15 19:53:55 +0600

    Middleware Tutorial With Example.
    Tutorial Web Address:
    https://www.itsolutionstuff.com/post/laravel-57-middleware-tutorial-with-exampleexample.html

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get("check-md", ["uses"=>"HomeController@checkMD", "middleware"=>"checkType"]);
