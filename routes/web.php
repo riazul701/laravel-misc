cc351c219392131e0bdd7a5a6a085c3c577e6c4e
Riazul Hasan <riazul701@gmail.com>
2020-07-15 18:07:20 +0600

    Laravel 7/6 Resource Route and Controller Tutorial.
    Tutorial Web Address:
    https://www.itsolutionstuff.com/post/laravel-6-resource-route-and-controller-tutorialexample.html

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//Route::get('items',['as'=>'items.index','uses'=>'ItemController@index']);
+//Route::post('items/create',['as'=>'items.store','uses'=>'ItemController@store']);
+//Route::get('items/edit/{id}',['as'=>'items.edit','uses'=>'ItemController@edit']);
+//Route::patch('items/{id}',['as'=>'items.update','uses'=>'ItemController@update']);
+//Route::delete('items/{id}',['as'=>'items.destroy','uses'=>'ItemController@destroy']);
+//Route::get('items/{id}',['as'=>'items.view','uses'=>'ItemController@view']);
+
+Route::resource('items', 'ItemController');
