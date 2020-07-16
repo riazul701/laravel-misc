1c3ca964fa016fdec1905b1c0c0ebb94cc301814
Riazul Hasan <riazul701@gmail.com>
2020-07-16 20:41:39 +0600

    File Upload Tutorial Example.
    Tutorial Web Address:
    https://www.itsolutionstuff.com/post/laravel-6-file-upload-tutorial-exampleexample.html

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('file-upload', 'FileUploadController@fileUpload')->name('file.upload');
+Route::post('file-upload', 'FileUploadController@fileUploadPost')->name('file.upload.post');
