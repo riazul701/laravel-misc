2366b7999d3138b782b8b5a9543866c9ee04cd28
Riazul Hasan <riazul701@gmail.com>
2020-07-17 01:26:55 +0600

    Create Dummy Data using tinker factory.
    Tutorial Web Address:
    https://www.itsolutionstuff.com/post/laravel-5-create-dummy-data-using-tinker-factoryexample.html

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -17,12 +17,29 @@ use Illuminate\Support\Str;
 |
 */
 
-$factory->define(User::class, function (Faker $faker) {
+//$factory->define(User::class, function (Faker $faker) {
+//    return [
+//        'name' => $faker->name,
+//        'email' => $faker->unique()->safeEmail,
+//        'email_verified_at' => now(),
+//        'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+//        'remember_token' => Str::random(10),
+//    ];
+//});
+
+$factory->define(App\User::class, function (Faker $faker) {
+    static $password;
     return [
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
-        'email_verified_at' => now(),
-        'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+        'password' => $password ?: $password = bcrypt('secret'),
         'remember_token' => Str::random(10),
+    ];
+});
+
+$factory->define(App\Post::class, function (Faker $faker) {
+    return [
+        'title' => $faker->name,
+        'body' => $faker->text,
     ];
 });
