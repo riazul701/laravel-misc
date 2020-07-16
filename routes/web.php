721cfa84ec1711b1007c6653184a8847a4e6c9a9
Riazul Hasan <riazul701@gmail.com>
2020-07-16 19:49:59 +0600

    Ajax Image Upload Example.
    Tutorial Web Address:
    https://www.itsolutionstuff.com/post/laravel-6-ajax-image-upload-exampleexample.html

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('ajaxImageUpload', 'AjaxImageUploadController@ajaxImageUpload');
+Route::post('ajaxImageUpload', 'AjaxImageUploadController@ajaxImageUploadPost')->name('ajaxImageUpload');
