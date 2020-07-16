b1433bba2db3f911f526b9df0797c1250fb8984d
Riazul Hasan <riazul701@gmail.com>
2020-07-16 18:20:14 +0600

    Ajax Request Example.
    Tutorial Web Address:
    https://www.itsolutionstuff.com/post/laravel-6-ajax-request-exampleexample.html

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('ajaxRequest', 'AjaxController@ajaxRequest');
+Route::post('ajaxRequest', 'AjaxController@ajaxRequestPost')->name('ajaxRequest.post');
