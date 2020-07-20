982342f05da608b7c69696ccd424baa21acf1738
Riazul Hasan <riazul701@gmail.com>
2020-07-20 23:29:09 +0600

    Create Custom Route File in Laravel.
    Tutorial Web Address:
    https://www.itsolutionstuff.com/post/how-to-create-custom-route-file-in-laravelexample.html

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -46,7 +46,10 @@ class RouteServiceProvider extends ServiceProvider
 
         $this->mapWebRoutes();
 
-        //
+
+        $this->mapAdminRoutes();
+
+        $this->mapManagerRoutes();
     }
 
     /**
@@ -76,5 +79,33 @@ class RouteServiceProvider extends ServiceProvider
             ->middleware('api')
             ->namespace($this->namespace)
             ->group(base_path('routes/api.php'));
+    }
+
+    /**
+     * Define the "admin" routes for the application.
+     *
+     * These routes are typically stateless.
+     *
+     * @return void
+     */
+    protected function mapAdminRoutes()
+    {
+        Route::prefix('admin')
+            ->namespace($this->namespace)
+            ->group(base_path('routes/admin.php'));
+    }
+
+    /**
+     * Define the "user" routes for the application.
+     *
+     * These routes are typically stateless.
+     *
+     * @return void
+     */
+    protected function mapManagerRoutes()
+    {
+        Route::prefix('manager')
+            ->namespace($this->namespace)
+            ->group(base_path('routes/manager.php'));
     }
 }
