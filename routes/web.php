ec24676e0b6ba5ced4a3b0490d6a267ee600fa91
Riazul Hasan <riazul701@gmail.com>
2021-01-22 20:18:03 +0600

    Laravel One to Many Eloquent Relationship Tutorial.
    Tutorial Web Address:
    https://www.itsolutionstuff.com/post/laravel-one-to-many-eloquent-relationship-tutorialexample.html

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Post;
+use App\Comment;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +17,48 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('get-comment', function () {
+    $post = Post::find(1);
+
+    $comments = $post->comments;
+
+    dd($comments);
+});
+
+Route::get('get-post', function () {
+    $comment = Comment::find(1);
+
+    $post = $comment->post;
+
+    dd($post);
+});
+
+Route::get('create-comment', function () {
+    $post = Post::find(1);
+
+    $comment = new Comment;
+    $comment->comment = "Hi ItSolutionStuff.com";
+
+    $post = $post->comments()->save($comment);
+});
+
+Route::get('create-comment-2', function () {
+    $post = Post::find(1);
+
+    $comment1 = new Comment;
+    $comment1->comment = "Hi ItSolutionStuff.com Comment 1";
+
+    $comment2 = new Comment;
+    $comment2->comment = "Hi ItSolutionStuff.com Comment 2";
+
+    $post = $post->comments()->saveMany([$comment1, $comment2]);
+});
+
+Route::get('associate', function () {
+    $comment = Comment::find(1);
+    $post = Post::find(2);
+
+    $comment->post()->associate($post)->save();
 });
