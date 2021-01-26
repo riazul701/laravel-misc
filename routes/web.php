94952f6672aced9485cab94c1271c89f658dee17
Riazul Hasan <riazul701@gmail.com>
2021-01-26 06:56:56 +0600

    Laravel Eager Loading Tutorial.
    Tutorial Web Address:
    https://www.itsolutionstuff.com/post/laravel-eager-loading-tutorialexample.html

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Post;
+use App\Comment;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +17,26 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('display-post', function () {
+    $posts = Post::all();
+
+    foreach ($posts as $post) {
+        echo $post->name . '<br />';
+
+        $comments = $post->comments;
+        /* You can write loop again  */
+    }
+});
+
+Route::get('display-post-with-eager-loading', function () {
+    $posts = Post::with('comments')->get();
+
+    foreach ($posts as $post) {
+        echo $post->name . '<br />';
+
+        $comments = $post->comments;
+        /* You can write loop again  */
+    }
 });
